1040520356c92998874f81dc05f8f177f2faa368
Andrei Apanel <aposha84@gmail.com>
2025-01-06 17:34:10 +0100

    fix:import png

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,7 +3,7 @@ import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
 
-import decoration from '../../resources/img/vision.png';
+import decoration from '../../pictures/vision.png';
 // import Spinner from '../spinner/spinner';
 
 export default function App() {
