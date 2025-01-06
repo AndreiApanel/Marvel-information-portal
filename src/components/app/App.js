7f6a4311b261e212fa0a3f18fcfd62d5e022d1e3
Andrei Apanel <aposha84@gmail.com>
2025-01-06 01:17:06 +0100

    Marvel information portal: init project

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import AppHeader from '../appHeader/AppHeader';
+
+// import Spinner from '../spinner/spinner';
+
+export default function App() {
+	return (
+		<div className="app">
+			<AppHeader />
+		</div>
+	);
+}
