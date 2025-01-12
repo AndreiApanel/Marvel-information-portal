5f7044803cb9010a4f26932932ef2aeadb7b8069
Andrei Apanel <aposha84@gmail.com>
2025-01-12 20:04:12 +0100

    add: method getCharacter

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -9,7 +9,12 @@ class MarvelService {
 	};
 	getAllCharacters = () => {
 		return this.getResource(
-			'https://gateway.marvel.com:443/v1/public/characters?apikey=af8b96479082ac1c08e6472e3ae23078'
+			'https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=af8b96479082ac1c08e6472e3ae23078'
+		);
+	};
+	getCharacters = (id) => {
+		return this.getResource(
+			`https://gateway.marvel.com:443/v1/public/characters/${id}?apikey=af8b96479082ac1c08e6472e3ae23078`
 		);
 	};
 }
