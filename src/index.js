826c28a507ede48eb0b4be503f65039ba650dc18
Andrei Apanel <aposha84@gmail.com>
2025-01-12 19:51:29 +0100

    added in the file index.js a request component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './components/app/App';
-
+import MarvelService from './services/MarvelService';
 import './style/style.scss';
 const container = document.getElementById('root');
 const root = createRoot(container);
+const marvelServices = new MarvelService();
 
+marvelServices.getAllCharacters().then((res) => console.log(res));
 root.render(<App />);
