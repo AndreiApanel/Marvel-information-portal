f1bc22cfbc1f046e0b2472d0a2d8dd41c96d3da0
Andrei Apanel <aposha84@gmail.com>
2025-01-12 19:39:29 +0100

    add async function fetch/await

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,10 +1,17 @@
 class MarvelService {
-	async getResource(url) {
+	getResource = async (url) => {
 		let res = await fetch(url);
 
 		if (!res.ok) {
 			throw new Error(`Could not fetch ${url},status: ${res.status}`);
 		}
 		return await res.json();
-	}
+	};
+	getAllCharacters = () => {
+		return this.getResource(
+			'https://gateway.marvel.com:443/v1/public/characters?apikey=af8b96479082ac1c08e6472e3ae23078'
+		);
+	};
 }
+
+export default MarvelService;
