5b537b121f55672b9a532efa822ab665c60b1d85
Andrei Apanel <aposha84@gmail.com>
2025-01-16 20:30:27 +0100

    fix App component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,18 +1,10 @@
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
 
-<<<<<<< HEAD
-import decoration from '../../pictures/vision.png';
-// import Spinner from '../spinner/spinner';
-=======
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
 
 import decoration from '../../pictures/vision.png';
->>>>>>> randomchar
 
 export default function App() {
 	return (
