eebf455f5ac1364de6179ca7dab7c926fed7085f
Andrei Apanel <aposha84@gmail.com>
2025-01-23 15:00:28 +0100

    rebuild component on the class

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,50 +1,72 @@
+import { Component } from 'react';
 import './charList.scss';
 import abyss from '../../pictures/abyss.jpg';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import MarvelService from '../../services/MarvelService';
+class CharList extends Component {
+	state = {
+		charList: [],
+		loading: true,
+		error: false,
+	};
 
-export default function CharList() {
-	return (
-		<div className="char__list">
-			<ul className="char__grid">
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item char__item_selected">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-				<li className="char__item">
-					<img src={abyss} alt="abyss" />
-					<div className="char__name">Abyss</div>
-				</li>
-			</ul>
-			<button className="button button__main button__long">
-				<div className="inner">load more</div>
-			</button>
-		</div>
-	);
+	marvelService = new MarvelService();
+	componentDidMount() {
+		this.marvelService
+			.getAllCharacters()
+			.then(this.onCharListLoaded)
+			.catch(this.onError);
+	}
+	componentWillUnmount() {
+		clearInterval(this.timerId);
+	}
+	render() {
+		return (
+			<div className="char__list">
+				<ul className="char__grid">
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item char__item_selected">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+					<li className="char__item">
+						<img src={abyss} alt="abyss" />
+						<div className="char__name">Abyss</div>
+					</li>
+				</ul>
+				<button className="button button__main button__long">
+					<div className="inner">load more</div>
+				</button>
+			</div>
+		);
+	}
 }
+export default CharList;
