544e30fbde514595cb24d1ea009957e45e20b1a6
Andrei Apanel <aposha84@gmail.com>
2025-02-06 18:22:47 +0100

    add eventListner (onClick)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,31 +1,33 @@
+import { Component } from 'react';
 import AppHeader from '../appHeader/AppHeader';
-import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
-
-<<<<<<< HEAD
-import decoration from '../../pictures/vision.png';
-// import Spinner from '../spinner/spinner';
-=======
 import RandomChar from '../randomChar/RandomChar';
-import CharList from '../charList/CharList';
-import CharInfo from '../charInfo/CharInfo';
 
 import decoration from '../../pictures/vision.png';
->>>>>>> randomChar
 
-export default function App() {
-	return (
-		<div className="app">
-			<AppHeader />
-			<main>
-				<RandomChar />
-				<div className="char__content">
-					<CharList />
-					<CharInfo />
-				</div>
-				<img className="bg-decoration" src={decoration} alt="vision" />
-			</main>
-		</div>
-	);
+class App extends Component {
+	state = {
+		selectedChar: null,
+	};
+	onCharSelected = (id) => {
+		this.setState({ selectedChar: id });
+	};
+	render() {
+		return (
+			<div className="app">
+				<AppHeader />
+				<main>
+					<RandomChar />
+					<div className="char__content">
+						<CharList onCharSelected={this.onCharSelected} />
+						<CharInfo charId={this.state.selectedChar} />
+					</div>
+					<img className="bg-decoration" src={decoration} alt="vision" />
+				</main>
+			</div>
+		);
+	}
 }
+
+export default App;
