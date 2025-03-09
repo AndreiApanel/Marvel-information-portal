1fadd24a06aa2dab09a09743be0a0a98d3f7da47
Andrei Apanel <aposha84@gmail.com>
2025-03-09 18:24:38 +0100

    hiding the button

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -10,6 +10,7 @@ class CharList extends Component {
 		error: false,
 		newItemLoading: false,
 		offset: 210,
+		charEnded: false,
 	};
 
 	marvelService = new MarvelService();
@@ -29,11 +30,17 @@ class CharList extends Component {
 		this.setState({ newItemLoading: true });
 	};
 	onCharListLoaded = (newCharList) => {
+		let ended = false;
+		if (newCharList.length < 9) {
+			ended = true;
+		}
+
 		this.setState(({ offset, charList }) => ({
 			charList: [...charList, ...newCharList],
 			loading: false,
 			newItemLoading: false,
 			offset: offset + 9,
+			charEnded: ended,
 		}));
 	};
 
@@ -67,7 +74,8 @@ class CharList extends Component {
 	}
 
 	render() {
-		const { charList, loading, error, offset, newItemLoading } = this.state;
+		const { charList, loading, error, offset, newItemLoading, charEnded } =
+			this.state;
 		const items = this.renderItems(charList);
 
 		const errorMessage = error ? <ErrorMessage /> : null;
@@ -82,6 +90,7 @@ class CharList extends Component {
 				<button
 					className="button button__main button__long"
 					disabled={newItemLoading}
+					style={{ display: charEnded ? 'none' : 'block' }}
 					onClick={() => this.onRequest(offset)}
 				>
 					<div className="inner">load more</div>
