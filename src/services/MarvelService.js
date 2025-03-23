dec9cd3fefe7e550a7a419cec8a8c3722c541aa9
Andrei Apanel <aposha84@gmail.com>
2025-03-23 13:08:02 +0100

    fix discriptions on cards

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -27,7 +27,9 @@ class MarvelService {
 		return {
 			id: char.id,
 			name: char.name,
-			discription: char.discription,
+			discription: char.discription
+				? `${char.discription.slice(0, 210)}...`
+				: 'There is no description for this character',
 			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
 			homepage: char.urls[0].url,
 			wiki: char.urls[1].url,
