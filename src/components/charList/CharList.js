1d78bdc0045bd9b7197f2df42d00099fe21c1a92
Andrei Apanel <aposha84@gmail.com>
2025-03-23 11:58:42 +0100

    using the Ref tool to focus the cards, add propTypes

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 import './charList.scss';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -50,8 +51,19 @@ class CharList extends Component {
 			loading: false,
 		});
 	};
+	itemRefs = [];
+	setInputRef = (ref) => {
+		this.itemRefs.push(ref);
+	};
+	focusOnItem = (id) => {
+		this.itemRefs.forEach((item) =>
+			item.classList.remove('char__item_selected')
+		);
+		this.itemRefs[id].classList.add('char__item_selected');
+		this.itemRefs[id].focus();
+	};
 	renderItems(arr) {
-		const items = arr.map((item) => {
+		const items = arr.map((item, i) => {
 			let imgStyle = { objectFit: 'cover' };
 			if (
 				item.thumbnail ===
@@ -62,8 +74,18 @@ class CharList extends Component {
 			return (
 				<li
 					className="char__item"
+					ref={this.setInputRef}
 					key={item.id}
-					onClick={() => this.props.onCharSelected(item.id)}
+					onClick={() => {
+						this.props.onCharSelected(item.id);
+						this.focusOnItem(i);
+					}}
+					onKeyDown={(e) => {
+						if (e.key === ' ' || e.key === 'Enter') {
+							this.props.onCharSelected(item.id);
+							this.focusOnItem(i);
+						}
+					}}
 				>
 					<img src={item.thumbnail} alt={item.name} style={imgStyle} />
 					<div className="char__name">{item.name}</div>
@@ -99,5 +121,7 @@ class CharList extends Component {
 		);
 	}
 }
-
+CharList.propTypes = {
+	onCharSelected: PropTypes.func.isRequired,
+};
 export default CharList;
