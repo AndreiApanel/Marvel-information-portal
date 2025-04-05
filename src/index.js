9f6a873ae20a8bf4191e475022687c3ab3dfb899
Andrei Apanel <aposha84@gmail.com>
2025-04-05 15:03:52 +0200

    fixed package

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
+// import MarvelService from './services/MarvelService';
 import './style/style.scss';
 const container = document.getElementById('root');
 const root = createRoot(container);
-const marvelServices = new MarvelService();
+// const marvelServices = new MarvelService();
 
-marvelServices.getAllCharacters().then((res) => console.log(res));
-root.render(<App />);
+// marvelServices.getAllCharacters().then((res) => console.log(res));
+root.render(
+<App />
+);
+
