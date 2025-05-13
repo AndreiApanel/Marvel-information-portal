aa339e565471bde1290185606e11dd3f0843e2e1
Andrei Apanel <aposha84@gmail.com>
2025-05-13 20:22:37 +0200

    rework components of class. App.js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,37 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import AppHeader from '../appHeader/AppHeader';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 import decoration from '../../pictures/vision.png';
-class App extends Component {
-	state = {
-		selectedChar: null,
-	};
-	onCharSelected = (id) => {
-		this.setState({ selectedChar: id });
-	};
-	render() {
-		return (
-			<div className="app">
-				<AppHeader />
-				<main>
-					<ErrorBoundary>
-						<RandomChar />
-					</ErrorBoundary>
-					<div className="char__content">
-						<ErrorBoundary>
-							<CharList onCharSelected={this.onCharSelected} />
-						</ErrorBoundary>
-						<ErrorBoundary>
-							<CharInfo charId={this.state.selectedChar} />
-						</ErrorBoundary>
-					</div>
-					<img className="bg-decoration" src={decoration} alt="vision" />
-				</main>
-			</div>
-		);
-	}
-}
+const App = () => {
+  const [selectedChar, setChar] = useState(null);
+
+  const onCharSelected = id => {
+    setChar(id);
+  };
+
+  return (
+    <div className="app">
+      <AppHeader />
+      <main>
+        <ErrorBoundary>
+          <RandomChar />
+        </ErrorBoundary>
+        <div className="char__content">
+          <ErrorBoundary>
+            <CharList onCharSelected={onCharSelected} />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <CharInfo charId={selectedChar} />
+          </ErrorBoundary>
+          y
+        </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
+      </main>
+    </div>
+  );
+};
 
 export default App;
