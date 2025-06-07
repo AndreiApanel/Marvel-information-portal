7eab7a78b2bf74f5c923bfe6f1bf8bbcb40582bc
Andrei Apanel <aposha84@gmail.com>
2025-06-07 12:20:45 +0200

    remake MarvelService component

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,41 +1,40 @@
-class MarvelService {
-	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
-	_apiKey = 
-	_baseOffset = 210;
-	getResource = async (url) => {
-		let res = await fetch(url);
+import apiKey from '../variable/apiKey';
+import { useHttp } from '../hooks/http.hook';
+const useMarvelService = () => {
+  const { loading, error, request, clearError } = useHttp();
 
-		if (!res.ok) {
-			throw new Error(`Could not fetch ${url},status: ${res.status}`);
-		}
-		return await res.json();
-	};
-	getAllCharacters = async (offset = this._baseOffset) => {
-		const res = await this.getResource(
-			`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
-		);
-		return res.data.results.map(this._transformCharacter);
-	};
-	getCharacter = async (id) => {
-		const res = await this.getResource(
-			`${this._apiBase}characters/${id}?${this._apiKey}`
-		);
-		return this._transformCharacter(res.data.results[0]);
-	};
+  const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
 
-	_transformCharacter = (char) => {
-		return {
-			id: char.id,
-			name: char.name,
-			description: char.description
-				? `${char.description.slice(0, 210)}...`
-				: 'There is no description for this character',
-			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-			homepage: char.urls[0].url,
-			wiki: char.urls[1].url,
-			comics: char.comics.items,
-		};
-	};
-}
+  const _baseOffset = 210;
 
-export default MarvelService;
+  const getAllCharacters = async (offset = _baseOffset) => {
+    const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${apiKey}`);
+    return res.data.results.map(_transformCharacter);
+  };
+  const getCharacter = async id => {
+    const res = await request(`${_apiBase}characters/${id}?${apiKey}`);
+    return _transformCharacter(res.data.results[0]);
+  };
+
+  const _transformCharacter = char => {
+    return {
+      id: char.id,
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 210)}...`
+        : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+      comics: char.comics.items,
+    };
+  };
+  return {
+    getAllCharacters,
+    getCharacter,
+    loading,
+    error,
+  };
+};
+
+export default useMarvelService;
