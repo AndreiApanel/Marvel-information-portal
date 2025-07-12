130ee2c5db77546f266f76585810120b8dcd2e8b
Andrei Apanel <aposha84@gmail.com>
2025-07-12 10:21:19 +0200

    174. Navigation in the app, React Router v5+

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,11 @@
+import AppBanner from '../appBanner/appBanner';
+import ComicsList from '../comicsList/ComicsList';
+
+const ComicsPage = () => (
+  <>
+    <AppBanner />
+    <ComicsList />
+  </>
+);
+
+export default ComicsPage;
