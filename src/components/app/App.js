2eeee1ed20bf818814aaa63fad140bfebdb24ddf
Andrei Apanel <aposha84@gmail.com>
2025-07-20 14:34:57 +0200

    177. React Lazy(suspense...all components, optimisation )

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,17 +1,18 @@
 import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
-import {MainPage, ComicsPage, SingleComicPage} from '../pages';
 import AppHeader from '../appHeader/AppHeader';
-
+import Spinner from '../spinner/Spinner';
 const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 const App = () => {
   return (
     <Router>
       <div className="app">
         <AppHeader />
         <main>
-          <Suspense fallback={<span>Loading...</span>}>
+          <Suspense fallback={<Spinner />}>
             <Routes>
               <Route path="/" element={<MainPage />} />
               <Route path="/comics" element={<ComicsPage />} />
