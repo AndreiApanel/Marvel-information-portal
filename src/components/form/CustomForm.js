e72a5b3b4ef0afabcce310f45b23b77e02c6291e
Andrei Apanel <aposha84@gmail.com>
2025-08-04 22:37:58 +0200

    186. Home task. Make a Form.

--- src/components/form/CustomForm.js
+++ src/components/form/CustomForm.js
@@ -1,20 +1,19 @@
-
 import './form.scss';
 
 const CustomForm = () => {
-	return (
-		<>
-			<div className='custom__form__container'>
-			<form className='custom__form'>
-				<input className='custom__form__input' type='text' placeholder='Enter name' />
-				<button className='custom__form__button' type='submit'><span>FIND</span>
-
-				</button>
-			</form>
-			</div>
-		</>
-
-	);
-}
+  return (
+    <>
+      <div className='custom__form__container'>
+        <p className='custom__form__name'>Or find a character by name:</p>
+        <form className='custom__form'>
+          <input className='custom__form__input' type='text' placeholder='Enter name' />
+          <button className='button button__main' type='submit'>
+            <div className='inner'>Find</div>
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default CustomForm;
