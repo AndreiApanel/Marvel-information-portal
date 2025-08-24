3fc2a2c95955f823b45301202f96cc1d37c67189
Andrei Apanel <aposha84@gmail.com>
2025-08-24 21:30:40 +0200

    190. Analysing third-party library errors and focus issues

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,11 +1,11 @@
-import {useState, useEffect, useRef, createRef, useCallback} from 'react';
+import {useState, useEffect, useRef, createRef, useMemo, useCallback} from 'react';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
 import PropTypes from 'prop-types';
 import useMarvelService from '../../services/MarvelService';
+import './charList.scss';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import './charList.scss';
 
 const setContent = (process, Component, newItemLoading) => {
   switch (process) {
@@ -23,6 +23,8 @@ const setContent = (process, Component, newItemLoading) => {
 };
 
 const CharList = props => {
+  const {onCharSelected} = props;
+
   const [charList, setCharList] = useState([]);
   const [newItemLoading, setNewItemLoading] = useState(false);
   const [offset, setOffset] = useState(210);
@@ -38,15 +40,12 @@ const CharList = props => {
     },
     [getAllCharacters, setProcess],
   );
+
   useEffect(() => {
     onRequest(210, true);
   }, [onRequest]);
 
   const onCharListLoaded = newCharList => {
-    // const {logger, secondFunc} = await import('./someFunc.js');
-    // logger();
-    // secondFunc();
-
     let ended = false;
     if (newCharList.length < 9) {
       ended = true;
@@ -71,44 +70,57 @@ const CharList = props => {
     }
   };
 
-  const renderItems = arr => {
-    itemRefs.current = {};
+  const renderItems = useCallback(
+    arr => {
+      itemRefs.current = {};
 
-    return (
-      <TransitionGroup component='ul' className='char__grid'>
-        {arr.map((item, i) => {
-          const ref = createRef();
-          itemRefs.current[item.id] = ref;
-          const imgStyle = item.thumbnail.includes('image_not_available') ? {objectFit: 'unset'} : {objectFit: 'cover'};
-          return (
-            <CSSTransition key={item.id} timeout={500} classNames='item' nodeRef={ref}>
-              <li
-                className='char__item'
-                tabIndex={0}
-                ref={ref}
-                onClick={() => {
-                  props.onCharSelected(item.id);
-                  focusOnItem(i);
-                }}
-                onKeyDown={e => {
-                  if (e.key === ' ' || e.key === 'Enter') {
-                    props.onCharSelected(item.id);
-                    focusOnItem(item.id);
-                  }
-                }}>
-                <img src={item.thumbnail} alt={item.name} style={imgStyle} />
-                <div className='char__name'>{item.name}</div>
-              </li>
-            </CSSTransition>
-          );
-        })}
-      </TransitionGroup>
-    );
-  };
+      return (
+        <TransitionGroup component='ul' className='char__grid'>
+          {arr.map((item, i) => {
+            if (!itemRefs.current[item.id]) {
+              itemRefs.current[item.id] = createRef();
+            }
+            const ref = itemRefs.current[item.id];
+
+            const imgStyle = item.thumbnail.includes('image_not_available')
+              ? {objectFit: 'unset'}
+              : {objectFit: 'cover'};
+
+            return (
+              <CSSTransition key={item.id} timeout={500} classNames='item' nodeRef={ref} appear>
+                <li
+                  className='char__item'
+                  tabIndex={0}
+                  ref={ref}
+                  onClick={() => {
+                    onCharSelected(item.id); // ✅ use destructured prop
+                    focusOnItem(i);
+                  }}
+                  onKeyDown={e => {
+                    if (e.key === ' ' || e.key === 'Enter') {
+                      onCharSelected(item.id); // ✅ use destructured prop
+                      focusOnItem(item.id);
+                    }
+                  }}>
+                  <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+                  <div className='char__name'>{item.name}</div>
+                </li>
+              </CSSTransition>
+            );
+          })}
+        </TransitionGroup>
+      );
+    },
+    [onCharSelected],
+  );
+
+  const elements = useMemo(() => {
+    return setContent(process, () => renderItems(charList), newItemLoading);
+  }, [process, charList, renderItems, newItemLoading]);
 
   return (
     <div className='char__list'>
-      {setContent(process, () => renderItems(charList), newItemLoading)}
+      {elements}
       <button
         className='button button__main button__long'
         disabled={newItemLoading}
